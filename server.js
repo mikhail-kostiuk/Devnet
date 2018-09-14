c9b674f4895565c5437bd0f45e062e6b2d24d47c
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2018-09-14 12:32:33 +0300

    Add route files with express router

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 // Connect to MongoDB
 mongoose
-  .connect(keys.mongoURI)
+  .connect(
+    keys.mongoURI,
+    { useNewUrlParser: true },
+  )
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
@@ -12,6 +18,11 @@ mongoose
 const app = express();
 
 app.get('/', (req, res) => res.send('Hello'));
+
+// Use routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const PORT = process.env.PORT || 5000;
 
