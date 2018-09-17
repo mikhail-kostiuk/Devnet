b6299815abc6dbbd038e2547c91c7cb29ca9e83e
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2018-09-17 13:05:44 +0300

    Add jwt on login

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../../models/User');
+const keys = require('../../config/keys');
 
 const router = express.Router();
 
@@ -59,10 +61,18 @@ router.post('/login', (req, res) => {
 
     // Check password
     bcrypt.compare(password, user.password).then((isMatch) => {
-      if (!isMatch) {
+      if (isMatch) {
+        // User matched
+        // Create JWT payload
+        const payload = { id: user.id, name: user.username, avatar: user.avatar };
+
+        // Sign token
+        jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+          res.json({ success: true, token: `Bearer ${token}` });
+        });
+      } else {
         return res.status(400).json({ msg: 'Password incorrect' });
       }
-      return res.json({ msg: 'Success' });
     });
   });
 });
