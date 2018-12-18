2c6db51666fd9c97630676416e7a0af9ab7d8b83
Mikhail Kostiuk <mikhail.kostiuk@gmail.com>
2018-12-18 11:42:23 +0200

    Implement delete, like, unlike post

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -40,6 +40,60 @@ export const getPosts = () => {
   };
 };
 
+// Delete post
+export const deletePost = id => {
+  return dispatch => {
+    axios
+      .delete(`api/posts/${id}`)
+      .then(res => {
+        dispatch({
+          type: actionTypes.DELETE_POST,
+          payload: id
+        });
+      })
+      .catch(err => {
+        dispatch({
+          type: actionTypes.GET_ERROR,
+          payload: err.response.data
+        });
+      });
+  };
+};
+
+// Add like
+export const addLike = id => {
+  return dispatch => {
+    axios
+      .post(`api/posts/like/${id}`)
+      .then(res => {
+        dispatch(getPosts());
+      })
+      .catch(err => {
+        dispatch({
+          type: actionTypes.GET_ERROR,
+          payload: err.response.data
+        });
+      });
+  };
+};
+
+// Remove like
+export const removeLike = id => {
+  return dispatch => {
+    axios
+      .post(`api/posts/unlike/${id}`)
+      .then(res => {
+        dispatch(getPosts());
+      })
+      .catch(err => {
+        dispatch({
+          type: actionTypes.GET_ERROR,
+          payload: err.response.data
+        });
+      });
+  };
+};
+
 // Set loading state
 export const setPostLoading = () => {
   return { type: actionTypes.POST_LOADING };
--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -12,6 +12,11 @@ const postReducer = (state = initialState, action) => {
       return { ...state, posts: [action.payload, ...state.posts] };
     case actionTypes.GET_POSTS:
       return { ...state, posts: action.payload, loading: false };
+    case actionTypes.DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter(post => post._id !== action.payload)
+      };
     case actionTypes.POST_LOADING:
       return { ...state, loading: true };
     default:
